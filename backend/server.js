c89101e8feb233cd7d3cd5b62b9db99d11d99550
unknown <yender.umc@gmail.com>
2025-12-17 16:01:55 -0400

    version 2

--- backend/server.js
+++ backend/server.js
@@ -116,7 +116,7 @@ async function generarMensaje(nombre) {
     ⚠️ IMPORTANTE: ¡NO OLVIDES LOS EMOJIS NI LOS ESPACIOS ENTRE PÁRRAFOS! SON VITALES.
     `
             }],
-            temperature: 0.6,
+            temperature: 0.7,
         });
         return completion.choices[0].message.content.replace(/^"|"$/g, '');
     } catch (e) {
